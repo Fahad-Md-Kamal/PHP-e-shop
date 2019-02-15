c442723b5ed414d20706eb7e5ca7ddea467a3f97
MH Fahad <Faahad.hossain@gmail.com>
2019-02-16 00:49:05 +0600

    templeting done

---
+++ index.php
@@ -0,0 +1,43 @@
+
+<!-- head  -->
+<?php include_once"templating/head.php" ?>
+
+  <!-- Navigation -->
+<?php include_once"templating/main_nav.php" ?>
+ 
+
+  <!-- Page Content -->
+  <div class="container">
+
+    <div class="row">
+
+      <div class="col-lg-3">
+
+        <h1 class="my-4">Shop Name</h1>
+        <?php include_once"templating/side_bar.php" ?>
+
+
+      </div>
+      <!-- /.col-lg-3 -->
+
+      <div class="col-lg-9">
+      <?php include_once"templating/slider.php" ?>
+
+
+    <!-- product list -->
+      <?php include_once"templating/product_list.php" ?>
+
+        <!-- /.row -->
+
+      </div>
+      <!-- /.col-lg-9 -->
+
+    </div>
+    <!-- /.row -->
+
+  </div>
+  <!-- /.container -->
+
+  <!-- Footer -->
+  <?php include_once"templating/footer_area.php" ?>
+ 

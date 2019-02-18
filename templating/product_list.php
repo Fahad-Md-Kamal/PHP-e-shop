8a73384d9358b0f57c2a90cd963b9263723c9fba
Big Bug <bigbug@gmail.com>
2019-02-18 23:40:56 +0600

    Image veriable error fixed fixed

--- templating/product_list.php
+++ templating/product_list.php
@@ -12,7 +12,7 @@
 
 <div class="col-lg-4 col-md-6 mb-4">
   <div class="card h-100">
-    <a href="#"><img class="card-img-top" src="img/<?=$row['image']?>" alt=""></a>
+    <a href="#"><img class="card-img-top" src="img/<?=$row['Image']?>" alt=""></a>
     <div class="card-body">
       <h4 class="card-title">
         <a href="#"><?=$row['name']?></a>

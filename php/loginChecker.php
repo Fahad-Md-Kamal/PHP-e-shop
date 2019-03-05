99c419e349a6f654454b60c42de8f6711c57e6d6
Fahad-Md-Kamal <faahad.hossain@gmail.com>
2019-03-05 13:27:42 +0600

    block hacking attampts done
    blocking hacking attampts done

--- php/loginChecker.php
+++ php/loginChecker.php
@@ -6,6 +6,11 @@ if ($_POST) {
     $email = $_POST['email'];
     $pass = $_POST['pass'];
 
+    if (!isset($_SESSION['count'])) {
+        $_SESSION['count'] = 0;
+    }
+
+
     if (empty($email)) {
         $_SESSION['msg'] = "Please enter valid email address";
     }elseif (empty($pass)) {
@@ -16,14 +21,21 @@ if ($_POST) {
         $conn = DataBaseConnection();
 
         $sql = "SELECT * FROM users WHERE email ='$email' AND pass = '$pass' ";
-        if($result = $conn->query($sql)){
+        $result = $conn->query($sql);
+        if($result->num_rows > 0){
             foreach($result AS $row){
                 $_SESSION['userName']= $userName = $row['name'];
             }
             $_SESSION['loggedIn'] = true;
             $_SESSION['msg'] = "Hey<br> $userName <br>Welcome Back  ";
+            unset($_SESSION['count']);
         }else{
             $_SESSION['msg'] = "Invalid Username or Password";
+            $_SESSION['count']++;
+
+            if($_SESSION['count'] >= 3){
+                setcookie('loginCounter', true, time() + 10);
+            }
         }
     }
     header("location:../login.php");
